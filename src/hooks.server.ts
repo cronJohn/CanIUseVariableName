49826f147a30de2dda8166b8b4d918355278ff42
cronJohn <51012201+cronJohn@users.noreply.github.com>
2025-03-19 15:11:54 -0500

    Add deployment to GitHub Pages

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,11 @@
-/** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ event, resolve }) {
-  const response = await resolve(event, {
-    transformPageChunk: ({ html }) =>
-      html.replace(
-        '%unocss-svelte-scoped.global%',
-        'unocss_svelte_scoped_global_styles'
-      ),
-  })
-  return response
-}
+import { sequence } from '@sveltejs/kit/hooks';
+
+export const unocssInject = async ({ event, resolve }) => {
+	const response = await resolve(event, {
+		transformPageChunk: ({ html }) =>
+			html.replace('%unocss-svelte-scoped.global%', 'unocss_svelte_scoped_global_styles')
+	});
+	return response;
+};
+
+export const handle = sequence(unocssInject);
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,22 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const isDev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
+        appDir: 'app',
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true
-		})
+		}),
+        paths: {
+            base: isDev ? '' : process.env.BASE_PATH,
+        }
 	},
 };
 
