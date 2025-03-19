546db78aa19cdb39f8771f25303a02fb15a72c13
cronJohn <51012201+cronJohn@users.noreply.github.com>
2025-03-19 11:26:29 -0500

    Update UI animation for valid variable name

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -19,7 +19,7 @@
     }
 </style>
 
-<main class="flex min-h-screen flex-col mb-10">
+<main class="flex min-h-screen flex-col items-center mb-10">
     <section class="flex flex-col w-full mt-25vh mb-4 justify-center items-center">
         <h1 class="c-white font-main mb-3">Enter variable name</h1>
         <input type="text" autofocus bind:value={inputName} oninput={async () => {
@@ -28,10 +28,11 @@
         class="rounded border-none font-main w-300px font-size-1.5em text-center py-.2em" />
     </section>
 
-    <section class="flex flex-col justify-center items-center">
+    <section class="flex flex-wrap w-500px">
         {#each languages as language, i (language)}
-            <h2 class="c-white relative bg-#2a2a2b rounded w-300px text-center py-2.5 m-1.3 px-2 font-main"
-            transition:scale={{ start: 0.5, delay: (100 * i), duration: 400, easing: cubicOut }} 
+            <h2 class="c-white relative bg-#2a2a2b rounded flex-[1_0_200px] text-center py-2.5 m-1.3 px-2 font-main"
+            in:scale={{ start: 0, delay: (70 * i), duration: 300, easing: cubicOut }} 
+            out:scale={{ start: 0, duration: 300, easing: cubicOut }}
             animate:flip={{duration: 500 }}>{language}</h2>
         {/each}
     </section>
