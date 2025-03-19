8c9fc457deaa991d1ed2eb47b205762e2c032f0d
cronJohn <51012201+cronJohn@users.noreply.github.com>
2025-03-18 19:19:29 -0500

    Restructure code and put footer in +layout.svelte file

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	let { children } = $props();
+</script>
+
+{@render children()}
+
+<footer class="text-center bg-#4D4D4D">
+    <a href="https://www.flaticon.com/free-icons/code" title="code icons" class="c-white font-main">Favicon created by Royyan Wijaya - Flaticon</a>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import "/src/global.css";
     import {flip} from "svelte/animate";
     import {scale} from "svelte/transition";
+    import {cubicOut} from "svelte/easing";
 
     import { getAllValidLanguages } from "$lib/validators/utils";
 
@@ -18,23 +19,20 @@
     }
 </style>
 
-<main class="flex flex-col w-100vw mt-25vh mb-4 justify-center items-center">
-    <h1 class="c-white font-main mb-3">Enter variable name</h1>
-    <input type="text" autofocus bind:value={inputName} oninput={async () => {
-        languages = await getAllValidLanguages(inputName);
-    }}
-    class="rounded border-none font-main w-300px font-size-1.5em text-center py-.2em" />
+<main class="flex min-h-screen flex-col mb-10">
+    <section class="flex flex-col w-full mt-25vh mb-4 justify-center items-center">
+        <h1 class="c-white font-main mb-3">Enter variable name</h1>
+        <input type="text" autofocus bind:value={inputName} oninput={async () => {
+            languages = await getAllValidLanguages(inputName);
+        }}
+        class="rounded border-none font-main w-300px font-size-1.5em text-center py-.2em" />
+    </section>
+
+    <section class="flex flex-col justify-center items-center">
+        {#each languages as language, i (language)}
+            <h2 class="c-white relative bg-#2a2a2b rounded w-300px text-center py-2.5 m-1.3 px-2 font-main"
+            transition:scale={{ start: 0.5, delay: (100 * i), duration: 400, easing: cubicOut }} 
+            animate:flip={{duration: 500 }}>{language}</h2>
+        {/each}
+    </section>
 </main>
-
-<div class="flex w-100vw flex-col justify-center items-center mb-7">
-    {#each languages as language, i (language)}
-        <h2 class="c-white bg-#2a2a2b rounded w-300px text-center py-2.5 m-1.3 px-2 font-main"
-        in:scale={{ start: 0.5, delay: (100 * i), duration: 500 }}
-        animate:flip={{duration: 500 }}>{language}</h2>
-    {/each}
-</div>
-
-<footer class="w-100vw text-center bg-#4D4D4D">
-    <a href="https://www.flaticon.com/free-icons/code" title="code icons" class="c-white font-main">Favicon created by Royyan Wijaya - Flaticon</a>
-</footer>
-
