ce415bdf4841faa447866e6f346fe1e58a459fd2
cronJohn <51012201+cronJohn@users.noreply.github.com>
2025-03-25 11:44:26 -0500

    Add typewriter animation to title and slide in to input

---
+++ src/lib/components/typewriter.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+  let {text = "", speed = 100, done} = $props();
+
+  let displayedText = $state("");
+  let index = 0;
+
+  const type = () => {
+    if (index < text.length) {
+      displayedText += text.charAt(index);
+      index++;
+      setTimeout(type, speed);
+    } else {
+        done();
+    }
+  };
+
+  onMount(() => type());
+</script>
+
+<h1 class="c-white font-main overflow-hidden b-r-[2px_red] mb-3">{displayedText}</h1>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     import "/src/global.css";
     import {flip} from "svelte/animate";
-    import {scale} from "svelte/transition";
+    import {scale, slide} from "svelte/transition";
     import {cubicOut} from "svelte/easing";
 
     import { getAllValidLanguages } from "$lib/validators/utils";
     import { validatorLibrary } from "$lib/validators/langs";
+    import Typewriter from "$lib/components/typewriter.svelte";
+    import { onMount } from "svelte";
 
     var inputName: string = $state("");
 
@@ -14,13 +16,14 @@
 
     var displayValid: boolean = $state(true);
 
+    var animateIn: boolean = $state(false);
+
     // Since you can switch between valid and invalid languages, we need to keep track of the target length
     const targetLanguageLength = $derived(displayValid ? validLanguages.length: invalidLanguages.length);
 
     const getIconName = (language: string) => {
         return validatorLibrary[language].iconName || language.toLowerCase();
     }
-
 </script>
 
 <style>
@@ -39,15 +42,19 @@
 
 <main class="flex min-h-screen flex-col items-center mb-10">
     <section class="flex flex-col w-full mt-25vh justify-center items-center">
-        <h1 class="c-white font-main mb-3">Enter variable name</h1>
-        <input type="text" autofocus bind:value={inputName} oninput={async () => {
-            validLanguages = await getAllValidLanguages(inputName);
-        }}
-        class:invalid={inputName.length > 0 && targetLanguageLength == 0}
-        class:valid={targetLanguageLength > 0}
-        class="rounded border-none font-main w-300px font-size-1.5em text-center py-.2em" />
+        <Typewriter text="Enter variable name..." speed={50} done={() => animateIn = true} />
+        {#if animateIn}
+            <input type="text" autofocus bind:value={inputName} oninput={async () => {
+                validLanguages = await getAllValidLanguages(inputName);
+            }}
+            class:invalid={inputName.length > 0 && targetLanguageLength == 0}
+            class:valid={targetLanguageLength > 0}
+            in:slide={{ duration: 500, easing: cubicOut }}
+            class="rounded border-none font-main w-300px font-size-1.5em text-center py-.2em" />
+        {/if}
         <button class="relative mt-1 bg-transparent b-none rounded-md text-sm tracking-wider font-main
                        underline underline-offset-3"
+        title="Toggle between valid and invalid languages"
         class:c-green={displayValid}
         class:c-red={!displayValid}
         class:hidden={inputName.length == 0}
